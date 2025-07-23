9ac3423df419bd7848f47941d4423f4823d44cc0
walter <hqzhu0461@163.com>
2025-07-23 23:03:31 +0800

    增加increase按钮

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
-//import CountClient from './countClient';
-
-import { getCount } from '@/lib/count'
+import { getCount } from '@/lib/count';
+import CountClient from './countClient';
 
 export default async function Home() {
   // 服务端获取初始数据
@@ -8,7 +7,7 @@ export default async function Home() {
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <span>{String(count)}</span>
+      <CountClient initialCount={count} />
     </div>
   );
 }
