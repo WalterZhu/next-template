1f944bd6c384236fddb6ac78e0d379ab1609d48e
walter <hqzhu0461@163.com>
2025-07-30 18:49:59 +0800

    修改初始化数据

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import CountClient from '@/components/CountClient';
+import { getCount } from '@/lib/count';
 
 export default async function Home() {
   // 服务端获取初始数据
-  const count = 0;
+  const count = await getCount();
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
